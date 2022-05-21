62f31b2d6e190dadf64b7d82d757e2c0f77165bd
EngineeringWithRamaa <rkramadhevi@gmail.com>
2022-05-21 13:18:20 +0530

    10000 User records read from file, transformed and dumped into XAMPP MySQL

---
+++ src/main/java/com/engineeringwithramaa/etlspringbatchprocessing/batch/Writer.java
@@ -0,0 +1,21 @@
+package com.engineeringwithramaa.etlspringbatchprocessing.batch;
+
+import com.engineeringwithramaa.etlspringbatchprocessing.DAO.UserDAO;
+import com.engineeringwithramaa.etlspringbatchprocessing.entity.User;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class Writer implements ItemWriter<User> {
+    @Autowired
+    private UserDAO userDAO;
+
+    @Override
+    public void write(List<? extends User> users) throws Exception {
+        userDAO.saveAll(users);
+        System.out.println("Writer Logging - User Data loaded into H2 db " + users);
+    }
+}
