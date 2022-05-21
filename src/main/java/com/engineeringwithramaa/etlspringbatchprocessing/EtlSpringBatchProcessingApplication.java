9e5e7944b2d354ebf906a189e223d8cde6884930
EngineeringWithRamaa <rkramadhevi@gmail.com>
2022-05-21 10:17:51 +0530

    ETL Batch Processing Application - Step 1 - Setting up configurations

---
+++ src/main/java/com/engineeringwithramaa/etlspringbatchprocessing/EtlSpringBatchProcessingApplication.java
@@ -0,0 +1,13 @@
+package com.engineeringwithramaa.etlspringbatchprocessing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtlSpringBatchProcessingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EtlSpringBatchProcessingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/engineeringwithramaa/etlspringbatchprocessing/EtlSpringBatchProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.engineeringwithramaa.etlspringbatchprocessing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtlSpringBatchProcessingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
